e1a31e772a7e81e1438d1e19c3eed2aa65a385d3
Lina Park <100692678+parkshyuny@users.noreply.github.com>
2022-03-26 11:00:38 +0900

    Create project2.cpp

---
+++ 2022_03_Design_Pattern_in_CPP/Section_3/project2.cpp
@@ -0,0 +1,56 @@
+class Entity {
+  string fname;
+  public:
+    Entity(string name): fname(name) {}
+    virtual int getSize() = 0;
+    virtual void print() = 0;
+}
+  
+class Folder : public Entity {
+  vector<Entity*> v;
+  public:
+    Folder(string name): Entity(name) {}
+    void add(Entity* p) {
+      v.push_back(p);
+    }
+    virtual int getSize() {
+      //TODO
+    }
+    virtual void print() {
+      //TODO
+    }
+}
+
+class File : public Entity {
+  int fsize;
+  public:
+    File(int size, string name): fsize(size), Entity(name) {}
+    virtual int getSize() {
+      return fsize;
+    }
+    virtual void print() {
+      cout << "(" << fname << ", " << fsize << ")" << endl;
+    }
+}
+
+int main() {
+  Folder* rootFolder = new Folder("ROOT");
+  Folder* aaaaFolder = new Folder("AAAA");
+  Folder* bbbbFolder = new Folder("BBBB");
+  
+  File* file1 = new File("a.txt", 10);
+  File* file2 = new File("b.txt", 20);
+  File* file3 = new File("c.txt", 30);
+  File* file4 = new File("d.txt", 40);
+  
+  rootFolder->add(aaaaFolder);
+  rootFolder->add(bbbbFolder);
+  rootFolder->add(file1);
+  
+  cout << file1->getSize()      << endl;
+  cout << aaaaFolder->getSize() << endl;
+  cout << rootFolder->getSize() << endl;
+  
+  file1->print();
+  file2->print();
+}
